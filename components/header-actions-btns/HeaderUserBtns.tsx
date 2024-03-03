4ba6f733f618436fc7572e156064b89ba5bd69b0
Nikita Samoylov <nsam7655@gmail.com>
2024-03-03 21:35:09 +0300

    feat/ add redirect to main page if cart empty

--- components/header-actions-btns/HeaderUserBtns.tsx
+++ components/header-actions-btns/HeaderUserBtns.tsx
@@ -5,48 +5,65 @@ import { LuHeart } from "react-icons/lu";
 import { LuShoppingCart } from "react-icons/lu";
 import { RiUserLine } from "react-icons/ri";
 import { useAppSelector } from '@/hooks';
+import { notify } from '@/utils/popupMsg';
+import { ToastContainer } from 'react-toastify';
 import styles from './HeaderUserBtns.module.scss';
 import './userBtns_default.scss';
 import './favorites.png';
 
 const HeaderUserBtns: React.FC = () => {
   const cartItems = useAppSelector(state => state.cartItem);
+  const showMsg = () => {
+    notify('товары еще не добавлены')
+  };
 
   return (
     <ul className={ styles.userBtns }>
       <li className={ styles.userBtns__item }>
-        <button className={ styles.userBtns__item_icon }>
-          <Badge count={ 0 } offset={ [10, 10] } >
+        <button style={ { backgroundColor: 'transparent' } }>
+          <Badge count={ 0 }
+            offset={ [10, 10] }
+            className={ styles.userBtns__item_icon }
+          >
             <RiUserLine size={ 20 } color='grey' />
           </Badge>
+          <div className={ styles.userBtns__name }>
+            войти
+          </div>
         </button>
-        <Link className={ styles.userBtns__link } href="/">
-          войти
-        </Link>
       </li>
       <li className={ styles.userBtns__item }>
-        <button className={ styles.userBtns__item_icon }>
-          <Badge count={ 0 } size='small'>
+        <button style={ { backgroundColor: 'transparent' } }>
+          <Badge count={ 0 }
+            size='small'
+            className={ styles.userBtns__item_icon }
+          >
             <LuHeart size={ 20 } color='grey' />
           </Badge>
+          <div className={ styles.userBtns__name }>
+            избранное
+          </div>
         </button>
-        <Link className={ styles.userBtns__link } href="/">
-          избранное
-        </Link>
       </li>
       <li className={ styles.userBtns__item }>
         <Link href="/cart">
-          <button className={ styles.userBtns__item_icon }>
-            <Badge count={ cartItems.length } size='small'>
-              <LuShoppingCart size={ 20 } color='grey' />
+          <button style={ { backgroundColor: 'transparent' } }
+            disabled={ cartItems.length !== 0 ? false : true }
+          >
+            <Badge count={ cartItems.length }
+              size='small'
+              className={ styles.userBtns__item_icon }
+            >
+              <LuShoppingCart size={ 20 }
+                color='grey' />
             </Badge>
+            <div className={ styles.userBtns__name }>
+              корзина
+            </div>
           </button>
         </Link>
-        <Link className={ styles.userBtns__link } href="/">
-          корзина
-        </Link>
-      </li>
-    </ul>
+      </li >
+    </ul >
   )
 };
 
